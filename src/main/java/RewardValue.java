f86ea5825a065a7ea2b30bf42b23b13336810541
ab5380 <ab5380@rit.edu>
2024-06-03 13:03:11 -0400

    Finished Task 2 (Test: 4/4)

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,13 +1,17 @@
 
 public class RewardValue {
-    private double cash;
-    private int miles;
+
+    private final double cash;
+    private final int miles;
+    protected static final double MILES_TO_CASH = 0.0035;
+
+
     public RewardValue(double cash){
         this.cash = cash;
-        this.miles = (int)(cash/0.0035);
+        this.miles = (int)(cash/MILES_TO_CASH);
     }
     public RewardValue(int miles){
-        this.cash = miles*0.0035;
+        this.cash = miles * MILES_TO_CASH;
         this.miles = miles;
     }
     public double getCashValue() {
